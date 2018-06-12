7fb69a6ef904cbd84c3542ca10d551d01b2cbe78
apmalla <amulyaprasad.malla@gmail.com>
2018-06-13 00:45:47 +0530

    Create ecrlogin.sh

---
+++ ecrlogin.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+docker build -t spinnaker-ecs .
+docker images
+docker tag spinnaker-ecs:latest 12345678912.dkr.ecr.us-west-1.amazonaws.com/spinnaker-ecs:dev
+sh /root/token.sh > /root/ecrtoken
+token=$(cat /root/ecrtoken)
+echo $token |docker login -u AWS https://12345678912.dkr.ecr.us-west-1.amazonaws.com --password-stdin
